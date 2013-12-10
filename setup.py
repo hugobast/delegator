2292d2107e43320c4ace39dabce00c9f5008cbd6
Hugo Bastien <hugobast@gmail.com>
2013-12-10 12:00:37 -0500

    converting to a python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Delegate',
+    author='Hugo Bastien',
+    author_email='hugobast@gmail.com',
+    url='https://github.com/hugobast/delegate',
+    version='0.0.1',
+    packages=['delegate',],
+    license='MIT License',
+    long_description=open('README.txt').read(),
+)
