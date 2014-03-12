5ca1cc4a279ad1c833034cb0b9a84810628f2dba
Hugo Bastien <hugobast@gmail.com>
2014-03-12 18:20:06 -0400

    Updated to version 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='Hugo Bastien',
     author_email='hugobast@gmail.com',
     url='https://github.com/hugobast/delegator',
-    version='0.0.2',
+    version='0.0.3',
     packages=['delegator'],
     license='MIT License',
     long_description=open('README.txt').read(),
